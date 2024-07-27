a46ef2711e1971af0adf7c5cb78d14dfb335153a
RAVIBHARATHI P C <131471282+ravibharathi656@users.noreply.github.com>
2024-07-27 11:20:23 +0530

    Newly Updated

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,14 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 
-import { Button, setConfig, frappeRequest, resourcesPlugin } from 'frappe-ui'
+import {
+  Button,
+  Card,
+  Input,
+  setConfig,
+  frappeRequest,
+  resourcesPlugin,
+} from 'frappe-ui'
 
 let app = createApp(App)
 
@@ -14,4 +21,7 @@ app.use(router)
 app.use(resourcesPlugin)
 
 app.component('Button', Button)
+app.component('Card', Card)
+app.component('Input', Input)
+
 app.mount('#app')
